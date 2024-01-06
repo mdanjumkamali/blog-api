7efe136e80a803fc462fe8df0be0e53e20fbedc4
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-01-06 18:44:06 +0530

    Add Post Schema

---
+++ models/post.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    content: {
+      type: String,
+      require: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.Model("Post", postSchema);
--- models/user.model.js
+++ models/user.model.js
@@ -1,21 +1,24 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    require: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    avatar: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    require: true,
-  },
-  password: {
-    type: String,
-    require: true,
-  },
-  avatar: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
