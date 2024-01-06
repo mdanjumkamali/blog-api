a748b0399cb34356021c961c4407d0be73367e39
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-01-06 18:48:26 +0530

    Add Comment Schema

---
+++ models/comment.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      require: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    replies: [
+      {
+        content: { type: String, required: true },
+        author: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
