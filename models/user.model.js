d34966efde2878d89c4ca9052d7954158270297a
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-01-06 17:58:18 +0530

    Add User Schema

---
+++ models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  avatar: {
+    type: String,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
