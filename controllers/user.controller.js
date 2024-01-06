965b2f595acd982df2d977474f4e4f9996a4ccf6
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-01-06 19:17:03 +0530

    Add Login Controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,13 +7,15 @@ import { User } from "../models/user.model.js";
 export const Signup = async (req, res) => {
   const { name, email, password, avatar } = req.body;
   try {
+    // Find the user by email
     const existingUser = await User.findOne({ email });
-
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
 
+    // Hash the password
     const hashPass = await bcrypt.hash(password, 10);
 
+    // Create a new user
     const user = await User.create({
       name,
       email,
@@ -23,6 +25,42 @@ export const Signup = async (req, res) => {
 
     res.status(200).json({ user: user, message: "Register Successful" });
   } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// login
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    // Check if the password is correct
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res
+      .status(200)
+      .json({ token, user, message: "User logged in successfully" });
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
