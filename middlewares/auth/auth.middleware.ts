06a474831571dc047f698f6ad3f3c2563a9e09ae
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-05-06 12:29:52 +0530

    Add auth middleware

---
+++ middlewares/auth/auth.middleware.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from "express";
+import Jwt from "jsonwebtoken";
+import { prismaClient } from "../../client/db";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+      };
+    }
+  }
+}
+
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.cookies.authToken;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+    const decoded = Jwt.verify(token, process.env.JWT_SECRET!);
+    if (!decoded || typeof decoded !== "object" || !decoded.id) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    const user = await prismaClient.user.findUnique({
+      where: { id: decoded.id },
+    });
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized: User not found" });
+    }
+    req.user = {
+      id: user.id,
+      email: user.email,
+    };
+
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
